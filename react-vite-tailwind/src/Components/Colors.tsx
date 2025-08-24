fd6a93167ad215a90d3367d9211a26c29544301f
dvaghasiya766 <devvaghasiya455@gmail.com>
2025-08-24 16:16:09 +0530

    Colors

---
+++ react-vite-tailwind/src/Components/Colors.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+export const Colors: React.FC<{}> = () => {
+  return (
+    <div>
+      <p className="text-blue-50">Hello I'm learning Tailwind CSS...</p>
+      <p className="text-blue-100">Hello I'm learning Tailwind CSS...</p>
+      <p className="text-blue-200">Hello I'm learning Tailwind CSS...</p>
+      <p className="text-blue-300">Hello I'm learning Tailwind CSS...</p>
+      <p className="text-blue-400">Hello I'm learning Tailwind CSS...</p>
+      <p className="text-blue-500">Hello I'm learning Tailwind CSS...</p>
+      <p className="text-blue-600">Hello I'm learning Tailwind CSS...</p>
+      <p className="text-blue-700">Hello I'm learning Tailwind CSS...</p>
+      <p className="text-blue-800">Hello I'm learning Tailwind CSS...</p>
+      <p className="text-blue-900">Hello I'm learning Tailwind CSS...</p>
+      <p className="text-blue-950">Hello I'm learning Tailwind CSS...</p>
+
+      <p className="bg-red-50">Hello I'm learning Tailwind CSS...</p>
+      <p className="bg-red-100">Hello I'm learning Tailwind CSS...</p>
+      <p className="bg-red-200">Hello I'm learning Tailwind CSS...</p>
+      <p className="bg-red-300">Hello I'm learning Tailwind CSS...</p>
+      <p className="bg-red-400">Hello I'm learning Tailwind CSS...</p>
+      <p className="bg-red-500">Hello I'm learning Tailwind CSS...</p>
+      <p className="bg-red-600">Hello I'm learning Tailwind CSS...</p>
+      <p className="bg-red-700">Hello I'm learning Tailwind CSS...</p>
+      <p className="bg-red-800">Hello I'm learning Tailwind CSS...</p>
+      <p className="bg-red-900">Hello I'm learning Tailwind CSS...</p>
+      <p className="bg-red-950">Hello I'm learning Tailwind CSS...</p>
+
+      <p className="border-2 border-green-50">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="border-2 border-green-100">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="border-2 border-green-200">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="border-2 border-green-300">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="border-2 border-green-400">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="border-2 border-green-500">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="border-2 border-green-600">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="border-2 border-green-700">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="border-2 border-green-800">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="border-2 border-green-900">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="border-2 border-green-950">
+        Hello I'm learning Tailwind CSS...
+      </p>
+
+      <p className="outline-yellow-50 outline outline-2">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="outline-yellow-100 outline outline-2">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="outline-yellow-200 outline outline-2">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="outline-yellow-300 outline outline-2">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="outline-yellow-400 outline outline-2">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="outline-yellow-500 outline outline-2">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="outline-yellow-600 outline outline-2">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="outline-yellow-700 outline outline-2">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="outline-yellow-800 outline outline-2">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="outline-yellow-900 outline outline-2">
+        Hello I'm learning Tailwind CSS...
+      </p>
+      <p className="outline-yellow-950 outline outline-2">
+        Hello I'm learning Tailwind CSS...
+      </p>
+    </div>
+  );
+};
